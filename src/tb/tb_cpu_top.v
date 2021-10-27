72cba34c9e82cd62051a97c4489e25a098ffde36
Fu-Zhou-2019 <2716559421@qq.com>
2021-10-27 15:21:25 +0800

    10_28

--- src/tb/tb_cpu_top.v
+++ src/tb/tb_cpu_top.v
@@ -14,30 +14,37 @@ module tb_cpu_top;
     .clk(clk),
     .rst_n(rst_n)
     );
+    
+    initial begin
+        rst_n = 0;
+        pc_rtvec = `PC_SIZE'h0000_0080;
+        #30 rst_n = 1;
+        @(posedge clk)
+        #50000 $finish;
+    end 
     reg[31:0] instr_in;
     integer i;
     integer test_file;
     initial begin
-        test_file = $fopen("C:\\Users\\wen\\Desktop\\cpu\\SimpleCore\\test_instr\\test_insr\\rv32ui-p-and.bin","rb");
-        for(i=0; i<32769; i=i+1) begin
-            $display("%h", instr_in);
+        test_file = $fopen("C:\\Users\\wen\\Desktop\\rv32ui-p-and","rb");
+        for(i=0; i<3500; i=i+1) begin
+            //$display(instr_in);
             if($fread(instr_in, test_file))
-                u_cpu_top.u_srams.u_itcm_ram.u_itcm_gnrl_ram.mem_r[i] <= {instr_in[7:0],instr_in[15:8],instr_in[23:16],instr_in[31:24]};           
+                u_cpu_top.u_srams.u_itcm_ram.u_itcm_gnrl_ram.mem_r[i] <=  {instr_in[7:0],instr_in[15:8],instr_in[23:16],instr_in[31:24]};
             else
-                u_cpu_top.u_srams.u_itcm_ram.u_itcm_gnrl_ram.mem_r[i] <= 32'd0;
+                u_cpu_top.u_srams.u_itcm_ram.u_itcm_gnrl_ram.mem_r[i] <=32'd0;
         end
         $fclose(test_file);
     end
     
-    initial begin
-        rst_n = 0;
-        pc_rtvec =32'h00000000;
-        #35 rst_n = 1;
-        @(posedge clk)
-        #50000 $finish;
-    end 
-    
     initial clk = 0;
     always #5 clk = ~clk; 
-endmodule
+    
+    integer j;
+    initial begin
+       for(j=0; j<3500; j=j+1) begin
+            u_cpu_top.u_srams.u_dtcm_ram.u_dtcm_gnrl_ram.mem_r[j] = 32'd1;//0x0001
+          end 
+       end
 
+endmodule
